8b3f5271393f92d7e21664c024f5066437df4f67
Yinner Chilito <yinnermedina30@gmail.com>
2024-02-13 08:14:10 -0500

    fix: routes Initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import { NavLink, Outlet } from 'react-router-dom';
 import logo from './assets/react.svg';
-import { Suspense } from 'react';
 
 function App() {
   return (
     <>
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <div className="main-layout">
-          <nav>
-            <img src={logo} alt="React Logo" />
-            <ul>
-              <li>
-                <NavLink to="lazylayout">LazyLayout</NavLink>
-              </li>
-              <li>
-                <NavLink to="no-lazylayout">No-LazyLayout</NavLink>
-              </li>
-            </ul>
-          </nav>
-          <Outlet />
-        </div>
-      </Suspense>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink to="home">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="users">Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </div>
     </>
   );
 }
